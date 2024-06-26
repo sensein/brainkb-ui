db6c7237ff88dd892a9f79811bbca74b16892d98
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-06-26 11:23:23 -0400

    OAuth google, github login page added

---
+++ src/app/login/page.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import {
+    GoogleOAuthSignInButton,
+    GithubOAuthSignInButton
+} from "@/src/app/components/oauthloginbuttons";
+import { getServerSession } from "next-auth";
+import { redirect, useRouter } from "next/navigation";
+import { authOptions } from "@/lib/auth";
+export const metadata: Metadata = {
+    title:"Login",
+
+};
+export default async function OAuthLogin(){
+    const current_session = await getServerSession(authOptions);
+    //user is logged in so redirect to admin page
+    if (current_session) return redirect("/admin");
+
+    return (
+        <div className="main-holder-brainkb flex justify-center items-center min-h-screen">
+            <div className="flex flex-col items-center p-10 shadow-lg rounded-lg bg-white animate-fade-in">
+                <h1 className="text-4xl font-bold mb-4">Sign In</h1>
+                <GoogleOAuthSignInButton/>
+                <GithubOAuthSignInButton/>
+            </div>
+        </div>
+
+
+    );
+};
