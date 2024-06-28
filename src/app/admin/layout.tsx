ea20f081e30c5dbfc491af9c2040050062b30b8d
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-06-28 10:06:56 -0400

    admin pages + layout init

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import NavbarAdmin from "@/src/app/components/NavbarAdmin";
+import FooterAdmin from "@/src/app/components/FooterAdmin";
+import { getServerSession } from "next-auth";
+import SessionProvider from "@/src/app/components/SessionProvider";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+
+export default async  function AdminRootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+    const session = await getServerSession();
+
+  return (
+      <SessionProvider session={session}>
+          <header>
+              <NavbarAdmin/>
+          </header>
+          <div className="p-4 sm:ml-64">
+              <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+
+                  {children}
+              </div>
+
+              </div>
+
+
+
+    <footer>
+        @
+    </footer>
+</SessionProvider>
+
+)
+    ;
+}
