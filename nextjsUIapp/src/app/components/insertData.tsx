9f9fd28db39d7337565e3a314789ab2c19d618c9
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-07-09 12:27:31 -0400

    utility function to insert data via API

---
+++ nextjsUIapp/src/app/components/insertData.tsx
@@ -0,0 +1,27 @@
+
+export async function insertData(formData, endpoint) {
+  try {
+    const apiEndpointbase = process.env.NEXT_PUBLIC_API_ADMIN_HOST as string;
+    if (!apiEndpointbase) {
+      throw new Error('NEXT_PUBLIC_API_ADMIN_HOST is not defined in the environment variables');
+    }
+
+    const api_endpoint = `${apiEndpointbase}/${endpoint}`
+    console.log(api_endpoint);
+    const response = await fetch(`${api_endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok'+ response);
+    }
+
+    return await response;
+  } catch (error) {
+    throw new Error('Error: ' + error.message);
+  }
+}
