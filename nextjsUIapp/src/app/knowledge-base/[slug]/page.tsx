24f2b1e6951559f7dd179d83ea7f4ce08bc28143
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-07-26 15:10:34 -0400

    subtitle description added

--- nextjsUIapp/src/app/knowledge-base/[slug]/page.tsx
+++ nextjsUIapp/src/app/knowledge-base/[slug]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import {useState, useEffect} from 'react';
 import {getData} from "@/src/app/components/getData";
-import yaml from "@/src/app/test/pages-config.yml";
+import yaml from "@/src/app/components/config.yaml";
 import SideBarKBFromConfig from "@/src/app/components/SideBarKBFromConfig";
 
 const ITEMS_PER_PAGE = 50;
@@ -21,6 +21,7 @@ const KbIndividualPageAllData = (
     const [loading, setLoading] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
     const [pagetitle, setPageTitle] = useState("");
+    const [pagesubtitle, setSubPageTitle] = useState("");
 
     const fetchData = async () => {
         setLoading(true);
@@ -28,10 +29,14 @@ const KbIndividualPageAllData = (
         setData([]);
         setHeaders([]);
         setPageTitle(null);
+        setSubPageTitle(null);
         const page = yaml.pages.find((page) => page.slug === params.slug);
         const query_to_execute = page ? page.sparql_query : "";
+        console.log(query_to_execute);
         const page_title = page ? page.page : "";
+        const page_sub_title = page ? page.description : "";
         setPageTitle(page_title);
+        setSubPageTitle(page_sub_title);
 
         const queryParameter = {sparql_query: query_to_execute};
 
@@ -78,7 +83,7 @@ const KbIndividualPageAllData = (
         return (
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead
-                    className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    className="sticky top-0 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     {headers.map((header, index) => (
                         <th key={index} className="border border-gray-400 px-4 py-2">{header}</th>
@@ -91,7 +96,7 @@ const KbIndividualPageAllData = (
                         className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                         {headers.map((header, headerIndex) => (
                             <td key={headerIndex}
-                                className="px-6 py-4">{item[header]?.value}</td>
+                                className="px-6 py-4">{item[header]?.value.substring(item[header]?.value.lastIndexOf('/') + 1)}</td>
                         ))}
                     </tr>
                 ))}
@@ -104,65 +109,70 @@ const KbIndividualPageAllData = (
 
     return (
         <div className="kb-page-margin">
-            <SideBarKBFromConfig/>
+          <SideBarKBFromConfig/>
 
-            <div className="grid fix-left-margin grid-cols-1 ">
-                <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                    <p className="text-2xl text-gray-400 dark:text-gray-500 text-center">
-                        {pagetitle}
-                    </p>
-                </div>
+          <div className="grid fix-left-margin grid-cols-1 ">
+            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+              <div className="text-center">
+                <p className="text-2xl text-gray-400 dark:text-gray-500">
+                  {pagetitle}
+                </p>
+                <p className="text-gray-400 dark:text-gray-500">
+                  {pagesubtitle}
+                </p>
+              </div>
             </div>
-            <div className="grid fix-left-margin grid-cols-1">
+          </div>
+          <div className="grid fix-left-margin grid-cols-1">
 
 
-                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    {renderTable()}
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              {renderTable()}
 
-                    {data.length > 0 && (
-                        <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
-                             aria-label="Table navigation">
+              {data.length > 0 && (
+                  <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
+                       aria-label="Table navigation">
     <span
         className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
       Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of <span
         className="font-semibold text-gray-900 dark:text-white">{data.length}</span>
     </span>
-                            <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-                                <li>
-                                    <button
-                                        onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-                                        disabled={currentPage === 1}
-                                        className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >
-                                        Previous
-                                    </button>
-                                </li>
-                                {Array.from({length: totalPages}, (_, index) => (
-                                    <li key={index}>
-                                        <button
-                                            onClick={() => setCurrentPage(index + 1)}
-                                            className={`flex items-center justify-center px-3 h-8 leading-tight ${currentPage === index + 1 ? 'text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700' : 'text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700'} dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
-                                            aria-current={currentPage === index + 1 ? 'page' : undefined}
-                                        >
-                                            {index + 1}
-                                        </button>
-                                    </li>
-                                ))}
-                                <li>
-                                    <button
-                                        onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
-                                        disabled={currentPage === totalPages}
-                                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                                    >
-                                        Next
-                                    </button>
-                                </li>
-                            </ul>
-                        </nav>
-                    )}
+                    <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+                      <li>
+                        <button
+                            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                            disabled={currentPage === 1}
+                            className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                        >
+                          Previous
+                        </button>
+                      </li>
+                      {Array.from({length: totalPages}, (_, index) => (
+                          <li key={index}>
+                            <button
+                                onClick={() => setCurrentPage(index + 1)}
+                                className={`flex items-center justify-center px-3 h-8 leading-tight ${currentPage === index + 1 ? 'text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700' : 'text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700'} dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+                                aria-current={currentPage === index + 1 ? 'page' : undefined}
+                            >
+                              {index + 1}
+                            </button>
+                          </li>
+                      ))}
+                      <li>
+                        <button
+                            onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+                            disabled={currentPage === totalPages}
+                            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                        >
+                          Next
+                        </button>
+                      </li>
+                    </ul>
+                  </nav>
+              )}
 
-                </div>
             </div>
+          </div>
 
         </div>
     );
--- nextjsUIapp/src/app/knowledge-base/page.tsx
+++ nextjsUIapp/src/app/knowledge-base/page.tsx
@@ -1,24 +1,181 @@
-import type { Metadata } from "next";
-import {getServerSession} from "next-auth";
-import {authOptions} from "@/lib/auth";
-import {redirect} from "next/navigation";
-import ConfigureFormPage from "@/src/app/knowledge-base/kb-layoutpage";
-import SideBarKB from "@/src/app/components/SideBarKB";
+"use client";
+import {useState, useEffect} from 'react';
+import {getData} from "@/src/app/components/getData";
+import yaml from "@/src/app/components/config.yaml";
+import SideBarKBFromConfig from "@/src/app/components/SideBarKBFromConfig";
 
-export const metadata: Metadata = {
-    title:"Knowledge Base",
+const ITEMS_PER_PAGE = 50;
 
-};
+const KnowledgeBase = (
+    {
+        params,
+    }: {
+        params: {
+            slug: string;
+        }
+    }
+) => {
+    const [data, setData] = useState<any[]>([]);
+    const [headers, setHeaders] = useState<string[]>([]);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pagetitle, setPageTitle] = useState("");
+    const [pagesubtitle, setSubPageTitle] = useState("");
 
-export default async function KnowledgeBase(){
-        const current_session = await getServerSession(authOptions);
-    //user is logged in so redirect to admin page
-    if (current_session) return redirect("/admin");
+    const fetchData = async () => {
+        setLoading(true);
+        setError(null);
+        setData([]);
+        setHeaders([]);
+        setPageTitle(null);
+        setSubPageTitle(null);
+        const page = yaml.pages.find((page) => page.slug === "default");
+        const query_to_execute = page ? page.sparql_query : "";
+        console.log(query_to_execute);
+        const page_title = page ? page.page : "";
+        const page_sub_title = page ? page.description : "";
+        setPageTitle(page_title);
+        setSubPageTitle(page_sub_title);
+
+        const queryParameter = {sparql_query: query_to_execute};
+
+        const baseurl = process.env.NEXT_PUBLIC_API_ADMIN_HOST;
+        const endpoint = process.env.NEXT_PUBLIC_API_QUERY_ENDPOINT;
+
+        console.log('Fetching data with parameters:', queryParameter, endpoint, baseurl);
+
+        try {
+            const response = await getData(queryParameter, endpoint, baseurl);
+            console.log('Raw response:', response);
+
+            if (response.status === 'success' && response.message?.results?.bindings) {
+                const bindings = response.message.results.bindings;
+                const vars = response.message.head.vars;
+                console.log('Bindings:', bindings);
+                console.log('Vars:', vars);
+                setHeaders(vars);
+                setData(bindings);
+            } else {
+                console.error('Unexpected response format:', response);
+                setError("Invalid data format");
+            }
+        } catch (e) {
+            const error = e as Error;
+            console.log('Error fetching data:', error.message);
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const renderTable = () => {
+        if (!data || !Array.isArray(data) || data.length === 0) return null;
+
+        const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+        const endIndex = startIndex + ITEMS_PER_PAGE;
+        const currentPageData = data.slice(startIndex, endIndex);
+
+        return (
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead
+                    className="sticky top-0 text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    {headers.map((header, index) => (
+                        <th key={index} className="border border-gray-400 px-4 py-2">{header}</th>
+                    ))}
+                </tr>
+                </thead>
+                <tbody>
+                {currentPageData.map((item, index) => (
+                    <tr key={index}
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        {headers.map((header, headerIndex) => (
+                            <td key={headerIndex}
+                                className="px-6 py-4">{item[header]?.value.substring(item[header]?.value.lastIndexOf('/') + 1)}</td>
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        );
+    };
+
+    const totalPages = Math.ceil(data.length / ITEMS_PER_PAGE);
+
     return (
         <div className="kb-page-margin">
-            <SideBarKB/>
-            <ConfigureFormPage/>
-        </div>
+            <SideBarKBFromConfig/>
 
+            <div className="grid fix-left-margin grid-cols-1 ">
+                <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                    <div className="text-center">
+                        <p className="text-2xl text-gray-400 dark:text-gray-500">
+                            {pagetitle}
+                        </p>
+                        <p className="text-gray-400 dark:text-gray-500">
+                            {pagesubtitle}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="grid fix-left-margin grid-cols-1">
+
+
+                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    {renderTable()}
+
+                    {data.length > 0 && (
+                        <nav className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
+                             aria-label="Table navigation">
+    <span
+        className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+      Showing <span className="font-semibold text-gray-900 dark:text-white">1-10</span> of <span
+        className="font-semibold text-gray-900 dark:text-white">{data.length}</span>
+    </span>
+                            <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+                                <li>
+                                    <button
+                                        onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                                        disabled={currentPage === 1}
+                                        className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                                    >
+                                        Previous
+                                    </button>
+                                </li>
+                                {Array.from({length: totalPages}, (_, index) => (
+                                    <li key={index}>
+                                        <button
+                                            onClick={() => setCurrentPage(index + 1)}
+                                            className={`flex items-center justify-center px-3 h-8 leading-tight ${currentPage === index + 1 ? 'text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700' : 'text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700'} dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+                                            aria-current={currentPage === index + 1 ? 'page' : undefined}
+                                        >
+                                            {index + 1}
+                                        </button>
+                                    </li>
+                                ))}
+                                <li>
+                                    <button
+                                        onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+                                        disabled={currentPage === totalPages}
+                                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 disabled:opacity-50 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                                    >
+                                        Next
+                                    </button>
+                                </li>
+                            </ul>
+                        </nav>
+                    )}
+
+                </div>
+            </div>
+
+        </div>
     );
-}
+};
+
+export default KnowledgeBase;
