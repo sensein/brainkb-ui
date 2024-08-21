163ce3a7e849af6d094649cfce99cac958cddd71
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-08-21 16:19:49 +0545

    updated detail page - individual

--- nextjsUIapp/src/app/knowledge-base/[slug]/[id]/page.tsx
+++ nextjsUIapp/src/app/knowledge-base/[slug]/[id]/page.tsx
@@ -1,57 +1,120 @@
 "use client";
 import SideBarKBFromConfig from "@/src/app/components/SideBarKBFromConfig";
-import {useEffect} from "react";
-import  enititycardmapperconfig  from '@/src/app/components/enititycardmapper.yaml';
-import yaml from 'js-yaml';
-const IndividualEntityPage = (
-    {
-        params,
-    }: {
-        params: {
-            slug: string;
-            id: string
-        }
-    }
-) => {
-    const loadData = async () => {
-            try {
-                // Fetch the mapper file
-                const page = enititycardmapperconfig.EntityViewCardsMaper.find((page) => page.slug === params.slug);
-                const filename = page ? page.filename : "";
+import { useEffect, useState } from "react";
+import enititycardmapperconfig from '@/src/app/components/enititycardmapper.yaml';
+import {getData} from "@/src/app/components/getData";
 
-                //read entity card design models dynamically
-                const data = await import(`@/src/app/components/${filename}`);
-                console.log('All YAML Data -- import:', data);
-                // console.log('All YAML Data -- import:', data.default.pages.find((page) => page.slug === "default"));
+const IndividualEntityPage = ({ params }) => {
+    const { slug, id } = params;
+    const [mainCardTitle, setMainCardTitle] = useState("");
+    const [mainCardDescription, setMainCardDescription] = useState("");
+    const [extractedBoxes, setExtractedBoxes] = useState([]);
+    const [data, setData] = useState<any[]>([]);
 
-            } catch (error) {
-                console.error('Failed to fetch YAML data:', error);
+    const baseurl = process.env.NEXT_PUBLIC_API_ADMIN_HOST || "http://3.134.90.242:8010";
+    const endpoint = process.env.NEXT_PUBLIC_API_QUERY_ENDPOINT || "query/sparql"; //default is "query/sparql"
+    const queryParameter = {sparql_query: decodeURIComponent(id)};
+    const fetchData = async () => {
+
+
+
+        try {
+            const response = await getData(queryParameter, endpoint, baseurl);
+
+
+            if (response.status === 'success' && response.message?.results?.bindings) {
+                const bindings = response.message.results.bindings;
+                const vars = response.message.head.vars;
+
+
+                setData(bindings);
+            } else {
+
+                setError("Invalid data format");
             }
+        } catch (e) {
+            const error = e as Error;
+
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+
+    };
+
+    const loadData = async () => {
+        setMainCardTitle("");
+        setData([]);
+        setMainCardDescription("");
+
+        try {
+            // Fetch the mapper file
+            const page = enititycardmapperconfig.EntityViewCardsMaper.find((page) => page.slug === slug);
+            const filename = page ? page.filename : "";
+
+            // Read entity card design models dynamically
+            const model_data = await import(`@/src/app/components/${filename}`);
+            const extracted_data = model_data.default;
+
+            // Set card title and description
+            setMainCardTitle(extracted_data?.name || "");
+            setMainCardDescription(extracted_data?.description || "");
+            setExtractedBoxes(extracted_data?.boxes || []);
+            console.log(extracted_data?.boxes);
+        } catch (error) {
+            console.error('Failed to fetch YAML data or Query:', error);
+        }
     };
 
     useEffect(() => {
         loadData();
     }, []);
 
+
     return (
         <div className="kb-page-margin">
-            <SideBarKBFromConfig/>
-
-            <div className="grid fix-left-margin grid-cols-1 ">
-                <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                    <div className="text-center">
-                        <p className="text-2xl text-gray-400 dark:text-gray-500">
-                            {params.slug}
-                        </p>
-                        <p className="text-gray-400 dark:text-gray-500">
-                           {params.id}
-                        </p>
+            <SideBarKBFromConfig />
+            <div className="grid fix-left-margin grid-cols-1">
+                <div className="w-full bg-white shadow-md rounded-lg overflow-hidden">
+                    <div className="p-4">
+                        <h2 className="text-xl font-bold">{mainCardTitle}</h2>
+                        <p className="text-gray-700">{mainCardDescription}</p>
+                    </div>
+                    <div className="p-4 border-t border-gray-200 flex">
+                        {extractedBoxes.map((entitycards, index) => (
+                            entitycards.cardtype === "card" ? (
+                                <div key={index} className="text-gray-700 text-base space-x-4">
+                                    {entitycards.slug === "summarybox" ? (
+                                        <div className="w-full bg-white shadow-md rounded-lg overflow-hidden">
+                                            <div className="bg-gray-400 p-4">
+                                                <h2 className="text-white text-xl font-bold">{
+                                                    decodeURIComponent(id).substring(decodeURIComponent(id).lastIndexOf('/') + 1)
+                                                }
+                                                </h2>
+                                            </div>
+                                            <div className="p-4">
+                                                <p className="text-gray-700">{entitycards.description}</p>
+                                                <p className="text-gray-700">{entitycards.sparl_query.replace(/\{0\}/g, decodeURIComponent(id))}</p>
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <div className="max-w-screen-md w-full bg-white shadow-md rounded-lg overflow-hidden">
+                                            <div className="bg-gray-400 p-4">
+                                                <h2 className="text-white text-xl font-bold">{entitycards.name}</h2>
+                                            </div>
+                                            <div className="p-4">
+                                                <p className="text-gray-700">{entitycards.description}</p>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            ) : (
+                                <div key={index} className="text-gray-700 text-base">no card</div>
+                            )
+                        ))}
                     </div>
                 </div>
             </div>
-
-
-
         </div>
     );
 };
