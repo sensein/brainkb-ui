dcec01f6a4e385c51ebbcb2332ad1231f2c97f0e
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-11-27 12:36:00 -0500

    Updated login page to include SignInButtons

--- nextjsUIapp/src/app/login/page.tsx
+++ nextjsUIapp/src/app/login/page.tsx
@@ -1,27 +1,28 @@
 import type { Metadata } from "next";
-import { 
-    GithubOAuthSignInButton
-} from "@/src/app/components/oauthloginbuttons";
 import { getServerSession } from "next-auth";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
+import SignInButtons from  "@/src/app/components/SignInButtons"; // Import the client-side sign-in buttons
+
 export const metadata: Metadata = {
-    title:"Login",
-
+  title: "Login",
 };
-export default async function OAuthLogin(){
-    const current_session = await getServerSession(authOptions);
-    //user is logged in so redirect to admin page
-    if (current_session) return redirect("/admin");
 
-    return (
-        <div className="main-holder-brainkb flex justify-center items-center min-h-screen">
-            <div className="flex flex-col items-center p-10 shadow-lg rounded-lg bg-white animate-fade-in">
-                <h1 className="text-4xl font-bold mb-4">Sign In</h1>
-                <GithubOAuthSignInButton/>
-            </div>
-        </div>
+export default async function OAuthLogin() {
+  const currentSession = await getServerSession(authOptions);
 
+  // If the user is logged in, redirect to the admin page
+  if (currentSession) {
+    redirect("/admin");
+  }
 
-    );
-};
+  return (
+    <div className="main-holder-brainkb flex justify-center items-center min-h-screen">
+      <div className="flex flex-col items-center p-10 shadow-lg rounded-lg bg-white animate-fade-in">
+        <h1 className="text-4xl font-bold mb-4">Sign In</h1>
+        {/* Render client-side sign-in buttons */}
+        <SignInButtons />
+      </div>
+    </div>
+  );
+}
