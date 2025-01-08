f0e281de833ff8f29d639d302737cd3f0b6bbf4b
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-01-08 14:46:01 -0500

    change the order of the menu options in navbar.

--- nextjsUIapp/src/app/components/Navbar.tsx
+++ nextjsUIapp/src/app/components/Navbar.tsx
@@ -69,14 +69,7 @@ const Navbar: React.FC = () => {
                             </Link>
                         </li>
 
-                        <li>
 
-                            <Link href="http://sensein.group/brainkbdocs" target="_blank"
-
-                                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                                Documentation
-                            </Link>
-                        </li>
                         <li>
                             <Link href="/playground"
                                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
@@ -88,6 +81,14 @@ const Navbar: React.FC = () => {
                             <Link href="/data-release"
                                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                 Data Release
+                            </Link>
+                        </li>
+                        <li>
+
+                            <Link href="http://sensein.group/brainkbdocs" target="_blank"
+
+                                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Documentation
                             </Link>
                         </li>
                         <li>
