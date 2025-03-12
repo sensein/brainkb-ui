80ae202276e7a8d05ee50ab08cebb4c9095e6772
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-03-12 08:25:31 -0400

    Updated GA tag

--- nextjsUIapp/src/app/layout.tsx
+++ nextjsUIapp/src/app/layout.tsx
@@ -28,27 +28,21 @@ export default async function RootLayout({
     return (
         <html lang="en">
         <head>
-            <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
-            {/* Google Tag Manager */}
+
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-3WBJJSZNMR"></script>
             <script dangerouslySetInnerHTML={{
                 __html: `
-(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W79DN38R');
-`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-3WBJJSZNMR');
+                `
             }}/>
-            {/* End Google Tag Manager */}
+            <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+
 
         </head>
         <body className={inter.className}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W79DN38R"
-                    height="0" width="0" style={{display: "none", visibility: "hidden"}}/>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
 
         <SessionProvider session={session}>
             <header>
