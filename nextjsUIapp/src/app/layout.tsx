500d6c76a8a469c28855d36b2fecffa36647dc1f
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-03-12 08:59:59 -0400

    fixed GA tag connected to tag manager

--- nextjsUIapp/src/app/layout.tsx
+++ nextjsUIapp/src/app/layout.tsx
@@ -29,36 +29,46 @@ export default async function RootLayout({
         <html lang="en">
         <head>
 
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-3WBJJSZNMR"></script>
-            <script dangerouslySetInnerHTML={{
-                __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-3WBJJSZNMR');
-                `
-            }}/>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=GTM-W79DN38R';
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W79DN38R');
+            `,
+                }}
+            />
             <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
 
 
         </head>
         <body className={inter.className}>
+        <noscript>
+            <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-W79DN38R"
+                height="0"
+                width="0"
+                style={{display: "none", visibility: "hidden"}}
+            ></iframe>
+        </noscript>
 
-        <SessionProvider session={session}>
-            <header>
-                <Navbar/>
-            </header>
+            <SessionProvider session={session}>
+                <header>
+                    <Navbar/>
+                </header>
 
 
-            <main className="flex min-h-screen flex-col">
+                <main className="flex min-h-screen flex-col">
 
-                {children}
-            </main>
+                    {children}
+                </main>
 
-            <footer>
-                <Footer/>
-            </footer>
-        </SessionProvider>
+                <footer>
+                    <Footer/>
+                </footer>
+            </SessionProvider>
 
         </body>
         </html>
