b6d3a40ac25396512d7feae3c4478d982416b708
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-03-16 10:17:32 -0400

    NER added to the menu.

--- nextjsUIapp/src/app/components/Navbar.tsx
+++ nextjsUIapp/src/app/components/Navbar.tsx
@@ -76,6 +76,12 @@ const Navbar: React.FC = () => {
                                 Playground
                             </Link>
                         </li>
+                        <li>
+                            <Link href="/ner"
+                                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                               NER
+                            </Link>
+                        </li>
 
                         <li>
                             <Link href="/tools-and-libraries"
