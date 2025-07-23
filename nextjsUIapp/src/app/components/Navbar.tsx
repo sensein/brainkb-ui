6797a06f7b72c8b49b2ef6aec6979023365012ff
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-07-23 12:16:58 -0400

    sandbox option added

--- nextjsUIapp/src/app/components/Navbar.tsx
+++ nextjsUIapp/src/app/components/Navbar.tsx
@@ -86,7 +86,7 @@ const Navbar: React.FC = () => {
                         <li>
                             <Link href="/see"
                                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                               SEE
+                                SEE
                             </Link>
                         </li>
 
@@ -111,6 +111,13 @@ const Navbar: React.FC = () => {
                                 Documentation
                             </Link>
                         </li>
+                        <li>
+                            <Link href="http://sandbox.brainkb.org" target="_blank"
+                                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                Sandbox
+                            </Link>
+                        </li>
+
                         <li>
                             <Link href="/about"
                                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
