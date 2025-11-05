bbe48fa789fc88d0451fa13c0f9e97d555e2cbe7
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-11-05 09:01:34 -0500

    renamed to user from admin

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Link from "next/link";
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/react";
+import SignInButtons from  "@/src/app/components/SignInButtons"; // Import the client-side sign-in buttons
 
 const Navbar: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -42,10 +43,9 @@ const Navbar: React.FC = () => {
                             )}
                         </>
                     ) : (
-                        <Link href="/login"  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Login
-                        </Link>
-
+                        <div className="bg-blue-600 dark:bg-blue-700 rounded-lg px-4 py-3 shadow-md">
+                            <SignInButtons />
+                        </div>
                     )}
 
                     <button data-collapse-toggle="navbar-sticky" type="button"
