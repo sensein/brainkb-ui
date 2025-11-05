a01549234919e370df0462f2299754ba602ff11d
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-11-05 10:08:25 -0500

    removed admin navbar

--- src/app/components/ConditionalNavbar.tsx
+++ src/app/components/ConditionalNavbar.tsx
@@ -2,19 +2,18 @@
 import { usePathname } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import Navbar from './Navbar';
-import NavbarAdmin from './NavBarAdmin';
 
 const ConditionalNavbar = () => {
   const pathname = usePathname();
   const { data: session } = useSession();
-  
-  // Check if we're on an admin page
-  const isAdminPage = pathname?.startsWith('/admin');
-  
-  // Show admin navbar only on admin pages
-  if (isAdminPage) {
-    return <NavbarAdmin />;
-  }
+//   we removed /admin as it's more of a user. NavBarAdmin is not being used.
+//   // Check if we're on an admin page
+//   const isAdminPage = pathname?.startsWith('/user');
+//
+//   // Show admin navbar only on admin pages
+//   if (isAdminPage) {
+//     return <NavbarAdmin />;
+//   }
   
   // Show regular navbar for all other pages (including home page)
   return <Navbar />;
