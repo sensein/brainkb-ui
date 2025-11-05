5f7c8605b928adf58d80644bef24b496027028ef
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-11-05 10:49:18 -0500

    added zIndex: 999999

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -49,7 +49,8 @@ const Navbar: React.FC = () => {
 
     return (
         <nav
-            className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600"
+            style={{ zIndex: 999999 }}>
             <div className="flex flex-wrap items-center justify-between mx-6 md:mx-8 lg:mx-12 p-4">
                 <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <Image 
