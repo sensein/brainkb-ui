1b8f9f38560db75a1ea5d73a329bedf90126d237
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-11-05 10:20:30 -0500

    redirect to user profile

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,34 +1,14 @@
-"use Client";
+
 import type { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-export const metadata: Metadata = {
-    title:"Admin",
-
-};
 export default  async function AdminIndex(){
   const session = await getServerSession();
   if (!session || !session.user) {
     redirect("/login");
   }
     return (
-        <>
-
-
-            <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                    This dashboard is currently under development.
-                    Note: SIE option will not work, we are updating the backend.
-                    <br/>
-                    If you have any feedback or suggestions, feel free to email me at{' '}
-                    <a href="mailto:tekraj@mit.edu" className="underline text-blue-400 hover:text-blue-600">
-                        tekraj@mit.edu
-                    </a>.
-                </p>
-            </div>
-
-
-        </>
+        redirect("/user/profile")
 
     );
 }
